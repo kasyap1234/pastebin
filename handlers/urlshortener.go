cdb1d05cbc46bc181fc847e078255db6ea8f6518
kasyap1234 <kasyap3103@gmail.com>
2024-05-07 19:02:39 +0530

    added base62 encoding

--- handlers/urlshortener.go
+++ handlers/urlshortener.go
@@ -1,9 +1,10 @@
-package handlers 
+package handlers
 
 import (
 	"crypto/md5"
-	"encoding/base64"
+	
 	"encoding/hex"
+	"math/big"
 )
 func GenerateShortURL(longURL string) (string,error){
 	// generating md5 hash for the long url 
@@ -16,5 +17,23 @@ func GenerateShortURL(longURL string) (string,error){
    base62Str :=base62Encode(hashString); 
    shortURL :="http://pastebin.com/"+base62Str
    return shortURL, nil ; 
-   
+
+}
+func base62Encode(input string) string {
+	const base62Chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	var result []byte
+
+	num := new(big.Int)
+	num.SetString(input, 16)
+
+	base := big.NewInt(62)
+	zero := big.NewInt(0)
+
+	for num.Cmp(zero) > 0 {
+		rem := new(big.Int)
+		num.DivMod(num, base, rem)
+		result = append([]byte{base62Chars[rem.Int64()]}, result...)
+	}
+
+	return string(result)
 }
