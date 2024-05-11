b2e778d3eb9448c4a88028c0f9544ca493f97236
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2024-05-11 18:28:59 +0530

    removed swagger

--- main.go
+++ main.go
@@ -5,27 +5,10 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/swaggo/files"
 	"github.com/kasyap1234/pastebin/database"
-	"github.com/swaggo/gin-swagger"
+	
 
 )
 
-// @title TODO APIs
-// @version 1.0
-// @description Testing Swagger APIs.
-// @termsOfService http://swagger.io/terms/
-// @contact.name API Support
-// @contact.url http://www.swagger.io/support
-// @contact.email support@swagger.io
-// @securityDefinitions.apiKey JWT
-// @in header
-// @name token
-// @license.name Apache 2.0
-// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-// @host localhost:8081
-// @BasePath /api/v1
-// @schemes http
-
-// @schemes http
 func main(){
  	database.ConnectDB();
  r :=gin.Default(); 
@@ -34,7 +17,7 @@ func main(){
  r.PUT("/updatePaste/:ID",handlers.UpdatePasteByID)
  r.DELETE("/deletePaste/:ID",handlers.DeletePasteByID)
  r.POST("/createShortURL/:longURL",handlers.CreateShortURL)
-r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
 
  r.Run(":8080")
   
